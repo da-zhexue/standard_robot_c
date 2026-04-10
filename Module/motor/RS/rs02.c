f031756efb9ae5c1f98e7edd52517f1cd0c8374b
da-zhexue <2152685808@qq.com>
2026-04-10 23:03:55 +0800

    fuck灵足

---
+++ Module/motor/RS/rs02.c
@@ -0,0 +1,257 @@
+// FUCK 灵足
+#include "rs02.h"
+
+void get_rs02_measure_mit(const uint8_t* rx_data, uint32_t id, void* arg);
+void get_rs02_measure_private(const uint8_t* rx_data, uint32_t id, void* arg);
+RS02_Status_t rs02_init(rs02_instance* rs02_ins, CAN_HandleTypeDef *hcan, const uint8_t motorid, const uint8_t masterid,
+    const uint8_t mode, const uint8_t protocol)
+{
+    rs02_ins->can_ins = BSP_CAN_Init(hcan);
+    rs02_ins->motorid = motorid;
+    rs02_ins->masterid = masterid;
+    rs02_ins->protocol = protocol;
+
+    if (protocol == RS02_PROTOCOL_MIT)
+    {
+        // 这个主机id的设置莫名其妙，MIT协议下回调函数的接收id是主机id而不是电机id
+        // 和bsp_can的代码相矛盾，所以我打算改为一个电机id对应一个主机id
+        rs02_change_masterid_mit(rs02_ins, masterid);
+        BSP_CAN_RegisterStdCallback(rs02_ins->can_ins, masterid, get_rs02_measure_mit, &rs02_ins->ecd);
+        rs02_setmode_mit(rs02_ins, mode); // 还有超绝必须先设置模式再使能，又要连发两条can
+        osDelay(1);
+        rs02_enable_mit(rs02_ins);
+        osDelay(1);
+    }
+    else if (protocol == RS02_PROTOCOL_PRIVATE)
+    {
+        // 主机id默认是0xFD
+        // 私有协议用扩展can，所有控制指令都通过写入参数实现，除了运控模式一个控制指令需要发两个can，不知道这sb电机的通信协议是怎么设计的
+        rs02_set_mitctrl_private(rs02_ins); // 依旧先设置模式再使能
+        BSP_CAN_RegisterExtCallback(rs02_ins->can_ins, ((RS02_CMD_CALLBACK << 24) | (masterid << 8) | motorid), 0x00FFFFFF, get_rs02_measure_private, &rs02_ins->ecd);
+        rs02_enable_private(rs02_ins);
+    }
+    return RS02_OK;
+}
+
+void rs02_change_masterid_mit(rs02_instance* rs02_ins, const uint8_t masterid)
+{
+    if (rs02_ins == NULL)
+        return;
+    if (rs02_ins->protocol != RS02_PROTOCOL_MIT)
+        return;
+
+    rs02_ins->masterid = masterid;
+    uint8_t tx_data[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xF0, 0x01};
+    tx_data[6] = masterid;
+    BSP_CAN_Transmit(rs02_ins->can_ins, rs02_ins->motorid, CAN_ID_STD, tx_data, 8);
+}
+
+void rs02_setmode_mit(const rs02_instance* rs02_ins, const uint8_t mode)
+{
+    if (rs02_ins == NULL)
+        return;
+    if (rs02_ins->protocol != RS02_PROTOCOL_MIT)
+        return;
+
+    uint8_t tx_data[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0xFC};
+    tx_data[6] = mode;
+    BSP_CAN_Transmit(rs02_ins->can_ins, rs02_ins->motorid, CAN_ID_STD, tx_data, 8);
+}
+
+void rs02_enable_mit(const rs02_instance* rs02_ins)
+{
+    if (rs02_ins == NULL)
+        return;
+    if (rs02_ins->protocol != RS02_PROTOCOL_MIT)
+        return;
+
+    const uint8_t tx_data[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFC};
+    BSP_CAN_Transmit(rs02_ins->can_ins, rs02_ins->motorid, CAN_ID_STD, tx_data, 8);
+}
+
+void rs02_disable_mit(const rs02_instance* rs02_ins)
+{
+    if (rs02_ins == NULL)
+        return;
+    if (rs02_ins->protocol != RS02_PROTOCOL_MIT)
+        return;
+
+    const uint8_t tx_data[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFD};
+    BSP_CAN_Transmit(rs02_ins->can_ins, rs02_ins->motorid, CAN_ID_STD, tx_data, 8);
+}
+
+void rs02_setzero_mit(const rs02_instance* rs02_ins)
+{
+    if (rs02_ins == NULL)
+        return;
+    if (rs02_ins->protocol != RS02_PROTOCOL_MIT)
+        return;
+
+    const uint8_t tx_data[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFE};
+    BSP_CAN_Transmit(rs02_ins->can_ins, rs02_ins->motorid, CAN_ID_STD, tx_data, 8);
+}
+
+void rs02_ctrl_pos_mit(const rs02_instance* rs02_ins, const fp32 angle, const fp32 speed)
+{
+    if (rs02_ins == NULL)
+        return;
+    if (rs02_ins->protocol != RS02_PROTOCOL_MIT)
+        return;
+
+    uint8_t tx_data[8] = {0};
+    memcpy(tx_data, &angle, 4);
+    memcpy(tx_data + 4, &speed, 4);
+    BSP_CAN_Transmit(rs02_ins->can_ins, rs02_ins->motorid, CAN_ID_STD, tx_data, 8);
+}
+
+void rs02_ctrl_speed_mit(const rs02_instance* rs02_ins, const fp32 speed, const fp32 current)
+{
+    if (rs02_ins == NULL)
+        return;
+    if (rs02_ins->protocol != RS02_PROTOCOL_MIT)
+        return;
+
+    uint8_t tx_data[8] = {0};
+    memcpy(tx_data, &speed, 4);
+    memcpy(tx_data + 4, &current, 4);
+    BSP_CAN_Transmit(rs02_ins->can_ins, rs02_ins->motorid, CAN_ID_STD, tx_data, 8);
+}
+
+// 更换通信协议后需要重新上电
+void rs02_change_protocol_mit(const rs02_instance* rs02_ins, const uint8_t protocol)
+{
+    if (rs02_ins == NULL)
+        return;
+    if (rs02_ins->protocol != RS02_PROTOCOL_MIT)
+        return;
+
+    const uint8_t tx_data[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, protocol, 0xFD};
+    BSP_CAN_Transmit(rs02_ins->can_ins, rs02_ins->motorid, CAN_ID_STD, tx_data, 8);
+}
+
+void get_rs02_measure_mit(const uint8_t* rx_data, uint32_t id, void* arg)
+{
+    if(rx_data == NULL || arg == NULL)
+        return;
+
+    rs02_ecd_t *ecd = (rs02_ecd_t*)arg;
+    ecd->motorid = rx_data[0];
+    ecd->ecd = (rx_data[1] << 8) | rx_data[2];
+    ecd->speed = (rx_data[3] << 4) | ((rx_data[4] >> 4) & 0x0F);
+    ecd->torch = ((rx_data[4] & 0x0F) << 8) | rx_data[5];
+    ecd->temperature = rx_data[6] << 8 | rx_data[7];
+}
+
+void rs02_enable_private(const rs02_instance* rs02_ins)
+{
+    if (rs02_ins == NULL)
+        return;
+    if (rs02_ins->protocol != RS02_PROTOCOL_PRIVATE)
+        return;
+
+    const uint32_t txid = ((RS02_CMD_ENABLE << 24) | (rs02_ins->masterid << 8) | rs02_ins->motorid);
+    const uint8_t tx_data[8] = {0};
+    BSP_CAN_Transmit(rs02_ins->can_ins, txid, CAN_ID_EXT, tx_data, 8);
+}
+
+void rs02_disable_private(const rs02_instance* rs02_ins)
+{
+    if (rs02_ins == NULL)
+        return;
+    if (rs02_ins->protocol != RS02_PROTOCOL_PRIVATE)
+        return;
+
+    const uint32_t txid = ((RS02_CMD_DISABLE << 24) | (rs02_ins->masterid << 8) | rs02_ins->motorid);
+    const uint8_t tx_data[8] = {0};
+    BSP_CAN_Transmit(rs02_ins->can_ins, txid, CAN_ID_EXT, tx_data, 8);
+}
+
+void rs02_setzero_private(const rs02_instance* rs02_ins)
+{
+    if (rs02_ins == NULL)
+        return;
+    if (rs02_ins->protocol != RS02_PROTOCOL_PRIVATE)
+        return;
+
+    const uint32_t txid = ((RS02_CMD_SETZERO << 24) | (rs02_ins->masterid << 8) | rs02_ins->motorid);
+    const uint8_t tx_data[8] = {0};
+    BSP_CAN_Transmit(rs02_ins->can_ins, txid, CAN_ID_EXT, tx_data, 8);
+}
+
+void rs02_setparam_private(const rs02_instance* rs02_ins, const uint16_t param_id, const fp32 param_value, const uint8_t param_mode)
+{
+    if (rs02_ins == NULL)
+        return;
+    if (rs02_ins->protocol != RS02_PROTOCOL_PRIVATE)
+        return;
+
+    const uint32_t txid = ((RS02_CMD_SETPARAM << 24) | (rs02_ins->masterid << 8) | rs02_ins->motorid);
+    uint8_t tx_data[8] = {0};
+    memcpy(tx_data, &param_id, 2);
+    if (param_mode == Set_parameter) {
+        memcpy(&tx_data[4], &param_value, 4);
+    } else if (param_mode == Set_mode) {
+        tx_data[4] = (uint8_t)param_value;
+        tx_data[5] = 0x00;
+        tx_data[6] = 0x00;
+        tx_data[7] = 0x00;
+    }
+    BSP_CAN_Transmit(rs02_ins->can_ins, txid, CAN_ID_EXT, tx_data, 8);
+}
+
+void rs02_set_mitctrl_private(const rs02_instance* rs02_ins)
+{
+    if (rs02_ins == NULL)
+        return;
+    if (rs02_ins->protocol != RS02_PROTOCOL_PRIVATE)
+        return;
+
+    rs02_setparam_private(rs02_ins, 0x7005, RS02_MODE_MIT, Set_mode);
+}
+
+void rs02_ctrl_move_private(const rs02_instance* rs02_ins, const fp32 angle, const fp32 speed, const fp32 torch, const fp32 kp, const fp32 kd)
+{
+    if (rs02_ins == NULL)
+        return;
+    if (rs02_ins->protocol != RS02_PROTOCOL_PRIVATE)
+        return;
+
+    const uint16_t angle_uint16 = (uint16_t)((angle + RS02_ECD_MAX) * RS02_UINT16_MAX / RS02_ECD_MAX);
+    const uint16_t speed_uint16 = (uint16_t)((speed + RS02_SPEED_MAX) * RS02_UINT16_MAX / (2 * RS02_SPEED_MAX));
+    const uint16_t torch_uint16 = (uint16_t)((torch + RS02_TORCH_MAX) * RS02_UINT16_MAX / (2 * RS02_TORCH_MAX));
+    const uint16_t kp_uint16 = (int16_t)(kp * RS02_UINT16_MAX / RS02_KP_MAX);
+    const uint16_t kd_uint16 = (int16_t)(kd * RS02_UINT16_MAX / RS02_KD_MAX);
+    const uint32_t txid = ((RS02_CMD_MOVECTRL << 24) | (torch_uint16 << 8) | rs02_ins->motorid);
+    uint8_t tx_data[8] = {0};
+    memcpy(tx_data, &angle_uint16, 2);
+    memcpy(tx_data + 2, &speed_uint16, 2);
+    memcpy(tx_data + 4, &kp_uint16, 2);
+    memcpy(tx_data + 6, &kd_uint16, 2);
+    BSP_CAN_Transmit(rs02_ins->can_ins, txid, CAN_ID_EXT, tx_data, 8);
+}
+
+// 更换通信协议后需要重新上电
+void rs02_change_protocol_private(const rs02_instance* rs02_ins, const uint8_t protocol)
+{
+    if (rs02_ins == NULL)
+        return;
+    if (rs02_ins->protocol != RS02_PROTOCOL_PRIVATE)
+        return;
+
+    const uint32_t txid = ((0x19 << 24) | (rs02_ins->masterid << 8) | rs02_ins->motorid);
+    const uint8_t tx_data[8] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, protocol, protocol};
+    BSP_CAN_Transmit(rs02_ins->can_ins, txid, CAN_ID_EXT, tx_data, 8);
+}
+
+void get_rs02_measure_private(const uint8_t* rx_data, uint32_t id, void* arg)
+{
+    if(rx_data == NULL || arg == NULL)
+        return;
+
+    rs02_ecd_t *ecd = (rs02_ecd_t*)arg;
+    ecd->ecd = (rx_data[0] << 8) | rx_data[1];
+    ecd->speed = (rx_data[2] << 8) | rx_data[3];
+    ecd->torch = (rx_data[4] << 8) | rx_data[5];
+    ecd->temperature = (rx_data[6] << 8) | rx_data[7];
+}
+
---
+++ Module/motor/RS/rs02.h
@@ -0,0 +1,95 @@
+#ifndef STANDARD_ROBOT_C_RS02_H
+#define STANDARD_ROBOT_C_RS02_H
+#include "can/bsp_can.h"
+#include "typedef.h"
+#include "user_lib.h"
+
+// 注意是将uint16_t[0, 65535]映射到[-RS02_ECD_MAX, RS02_ECD_MAX]，而不是[0, RS02_ECD_MAX], speed和torch同理
+#define RS02_UINT16_MAX 65535
+#define RS02_ECD_MAX (4*PI)
+#define RS02_SPEED_MAX 44.0f
+#define RS02_TORCH_MAX 17.0f
+#define RS02_KP_MAX 500.0f
+#define RS02_KD_MAX 5.0f
+#define RS02_TEMP_RATIO 0.1f
+
+#define Set_mode 'j'
+#define Set_parameter 'p'
+
+typedef enum
+{
+    RS02_MODE_MIT = 0, // MIT模式
+    RS02_MODE_POS = 1, // 位置控制
+    RS02_MODE_SPEED = 2, // 速度控制
+} RS02_CTRL_MODE;
+
+typedef enum
+{
+    RS02_PROTOCOL_PRIVATE = 0, // 私有协议
+    RS02_PROTOCOL_CANOPEN = 1, // CANopen协议
+    RS02_PROTOCOL_MIT = 2, // MIT协议
+} RS02_PROTOCOL;
+
+typedef enum
+{
+    RS02_CMD_GETID = 0x00,
+    RS02_CMD_MOVECTRL = 0x01,
+    RS02_CMD_CALLBACK = 0x02,
+    RS02_CMD_ENABLE = 0x03,
+    RS02_CMD_DISABLE = 0x04,
+    RS02_CMD_SETZERO = 0x06,
+    RS02_CMD_SETID = 0x07,
+    RS02_CMD_GETPARAM = 0x11,
+    RS02_CMD_SETPARAM = 0x12,
+    RS02_CMD_ERRORCB = 0x15,
+    RS02_CMD_SAVEPARAM = 0x16,
+    RS02_CMD_CHANGEBUAD = 0x17,
+    RS02_CMD_AUTOFEEDBACK = 0x18,
+    RS02_CMD_CHANGEPROTOCOL = 0x19
+} RS02_CMD_PRIVATE;
+
+typedef enum
+{
+    RS02_OK = 0,
+    RS02_ERROR_INVALID_PARAM = 1,
+    RS02_ALREADY_INITIALIZED = 2,
+    RS02_ERROR = 3
+} RS02_Status_t;
+
+typedef struct
+{
+    uint8_t motorid;
+    uint16_t ecd;
+    uint16_t speed;
+    uint16_t torch;
+    uint16_t temperature;
+    uint16_t last_ecd;
+}rs02_ecd_t;
+
+typedef struct
+{
+    CAN_Instance_t *can_ins;
+    uint8_t motorid;
+    uint8_t masterid;
+    rs02_ecd_t ecd;
+    RS02_CTRL_MODE mode;
+    RS02_PROTOCOL protocol;
+}rs02_instance;
+
+RS02_Status_t rs02_init(rs02_instance* rs02_ins, CAN_HandleTypeDef *hcan, uint8_t motorid,
+    uint8_t masterid, uint8_t mode, uint8_t protocol);
+void rs02_change_masterid_mit(rs02_instance* rs02_ins, uint8_t masterid);
+void rs02_setmode_mit(const rs02_instance* rs02_ins, uint8_t mode);
+void rs02_enable_mit(const rs02_instance* rs02_ins);
+void rs02_disable_mit(const rs02_instance* rs02_ins);
+void rs02_setzero_mit(const rs02_instance* rs02_ins);
+void rs02_ctrl_pos_mit(const rs02_instance* rs02_ins, fp32 angle, fp32 speed);
+void rs02_ctrl_speed_mit(const rs02_instance* rs02_ins, fp32 speed, fp32 current);
+void rs02_change_protocol_mit(const rs02_instance* rs02_ins, uint8_t protocol);
+void rs02_enable_private(const rs02_instance* rs02_ins);
+void rs02_disable_private(const rs02_instance* rs02_ins);
+void rs02_setzero_private(const rs02_instance* rs02_ins);
+void rs02_set_mitctrl_private(const rs02_instance* rs02_ins);
+void rs02_ctrl_move_private(const rs02_instance* rs02_ins, fp32 angle, fp32 speed, fp32 torch, fp32 kp, fp32 kd);
+void rs02_change_protocol_private(const rs02_instance* rs02_ins, uint8_t protocol);
+#endif //STANDARD_ROBOT_C_RS02_H
